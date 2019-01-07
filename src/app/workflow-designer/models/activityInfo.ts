bf23d27270c174ec51c73db54b69997af0b9617d
vishakh P. kumar <vishakh.p@claysys.com>
2019-01-07 09:03:27 -0800

    ActivityInfo

---
+++ src/app/workflow-designer/models/activityInfo.ts
@@ -0,0 +1,10 @@
+export class ActivityInfo {
+    name: string;
+    type: string;
+    parameteres: ParameterInfo[] = [];
+}
+
+export class ParameterInfo {
+    name: string;
+    type: string;
+}
---
+++ src/app/workflow-designer/services/workflowDesigner.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { ActivityInfo } from '../models/activityInfo';
+
+@Injectable()
+export class WorkflowDesigner {
+    public addActivity(activityInfo: ActivityInfo) {
+
+    }
+}
